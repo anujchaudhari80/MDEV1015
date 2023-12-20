08c6dd48b67e52006236292fe7685292b43229cb
Ilham Sheikh <ilhamsheikh@Ilhams-MacBook-Pro.local>
2023-12-20 10:42:33 -0500

    merging

--- Assignments/Assignment1/Navigation.tsx
+++ Assignments/Assignment1/Navigation.tsx
@@ -5,51 +5,62 @@ import OrderHistoryScreen from './OrderHistory';
 import CartScreen from './Cart';
 import ProfileScreen from './Profile';
 import Flex from './Home';
-import Icon from './components/Icon';
+// import Icon from './components/Icon';
+import Icon  from 'antd';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+
 
 const Tab = createBottomTabNavigator();
 
 type TabItem = {
   route: string;
   label: string;
-  type: keyof typeof Icons;
+  // type: keyof typeof Icons;
   activeIcon: string;
   inActiveIcon: string;
   component: React.ComponentType<any>;
+  iconLibrary: string;
 };
 
 const TabArr: TabItem[] = [
   {
     route: 'Home',
     label: 'Home',
-    type: 'Ionicons',
+    // type: 'Ionicons',
     activeIcon: 'home',
     inActiveIcon: 'home-outline',
     component: Flex,
+    iconLibrary: 'home'
   },
   {
     route: 'History',
     label: 'History',
-    type: 'Ionicons',
+    // type: 'Ionicons',
     activeIcon: 'book',
-    inActiveIcon: 'book-outline',
+    inActiveIcon: 'book',
     component: OrderHistoryScreen,
+    iconLibrary: 'book'
   },
   {
     route: 'Cart',
     label: 'Cart',
-    type: 'MaterialCommunityIcons',
-    activeIcon: 'cart',
-    inActiveIcon: 'cart-outline',
+    // type: 'MaterialCommunityIcons',
+    activeIcon: 'home',
+    inActiveIcon: 'home',
     component: CartScreen,
+    iconLibrary: 'home'
   },
   {
     route: 'Profile',
     label: 'Profile',
-    type: 'FontAwesome5',
-    activeIcon: 'user-alt',
+    // type: 'FontAwesome5',
+    activeIcon: 'user',
     inActiveIcon: 'user',
     component: ProfileScreen,
+    iconLibrary: 'user'
   },
 ];
 
@@ -71,12 +82,15 @@ const Navigation = () => {
             options={{
               tabBarLabel: item.label,
               tabBarIcon: ({ color, focused }) => (
-                <Icon
-                  type={item.type}
-                  name={focused ? item.activeIcon : item.inActiveIcon}
-                  color={color}
-                />
-              ),
+                // <Icon
+                //   type='home'
+                //   theme={focused ? 'filled' : 'outlined'}
+                // />
+                <AntDesign name="home" size={24} color={color} />
+                // <AntIcon name='minuscircleo' size={24} color='#66BB6A' />
+                // const IconComponent = focused ? item.iconLibrary : item.iconLibrary;
+                // return <IconComponent name={focused ? item.activeIcon : item.inActiveIcon} size={24} color={color} />;
+                ),
             }}
           />
         );
